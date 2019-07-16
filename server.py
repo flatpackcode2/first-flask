bb6bd27253daeb46258386be7cfd406852415352
flatpackcode2 <colinnoahpeter@gmail.com>
2019-07-16 16:32:51 +0800

    refactored template file

--- server.py
+++ server.py
@@ -1,16 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/<name>')
-def index(name):
-    hobbies = ['writing', 'reading', 'rowing', 'drawiing']
-    signed_in=True
-    name=name.upper()
-    return render_template('index.html', name=name, hobbies = hobbies, signed_in = signed_in)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/user/<username>')
-def show(username):
-    return f'Hi {username}'
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
